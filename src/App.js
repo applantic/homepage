4cb5a0567620f06f25d6ad030cfd352f66cf4e78
Kamil Dzieniszewski <kamil.dzieniszewski@daftcode.pl>
2019-03-01 18:46:13 +0100

    feat: add logo, email and background color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import logo from './assets/logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="container">
+        <header className="header">
+          <img src={logo} className="logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Applantic
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
+            className="link"
+            href="mailto:applantic@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            applantic@gmail.com
           </a>
         </header>
       </div>
