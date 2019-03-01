e69817fa24bcd61c0680d1216959b4d6b2d55554
Kamil Dzieniszewski <kamil.dzieniszewski@daftcode.pl>
2019-03-01 18:41:32 +0100

    Updates

---
+++ precache-manifest.7f179a9ab9d3f70813d4a1f6e63a6db1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "afc76f89b2e7cbfb9bc0",
+    "url": "/homepage/static/css/main.75b622eb.chunk.css"
+  },
+  {
+    "revision": "afc76f89b2e7cbfb9bc0",
+    "url": "/homepage/static/js/main.afc76f89.chunk.js"
+  },
+  {
+    "revision": "810d44d7fb3dbe398ef1",
+    "url": "/homepage/static/js/runtime~main.810d44d7.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/homepage/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "25a581414873b905b3a92aec4192470a",
+    "url": "/homepage/static/media/logo.25a58141.png"
+  },
+  {
+    "revision": "d6d8446106b9c88b95ef2402fd382d8c",
+    "url": "/homepage/index.html"
+  }
+];
