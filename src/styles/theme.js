e9efe819d7ec7e239261631a6dfa6a4b17cff62f
Kamil Dzieniszewski <kamil.dzieniszewski@daftcode.pl>
2019-03-30 00:10:38 +0100

    fix: gradient on header

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,8 +4,8 @@ export const createTheme = ({ primary, secondary }) => ({
   primary,
   secondary,
   colors,
-  primaryGradient: `linear-gradient(90deg, ${colors.darkPeach} 0%, ${
-    colors.peach
+  primaryGradient: `linear-gradient(90deg, ${colors.peach} 0%, ${
+    colors.darkPeach
   } 100%)`,
   secondaryGradient: `linear-gradient(90deg, ${colors.oceanBlue} 0%, ${
     colors.scampi
